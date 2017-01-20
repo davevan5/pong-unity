f80430c9ba2e08de9e9ca7c64bff11599e3a3172
ginja <davidevans1985@gmail.com>
2017-01-20 00:09:18 +0000

    Add basic components of game
    - Two player paddles
    - Pong ball
    - Invisible walls
    - Physics for ball/wall/players
    - Player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int PlayerNumber = 1;
+
+    public float Speed = 30;
+
+    private Rigidbody2D playerBody;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("Vertical" + PlayerNumber);
+        playerBody.velocity = new Vector2(0, v) * Speed;
+    }
+}
---
+++ Assets/Scripts/PongController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PongController : MonoBehaviour {
+
+    private Rigidbody2D pongBody;
+
+    void Start () {
+        pongBody = GetComponent<Rigidbody2D>();
+        pongBody.AddForce(new Vector2(700, 0));
+    }
+}
