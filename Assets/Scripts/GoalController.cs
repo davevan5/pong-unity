8bfbff6463279bc8307e99bf7c69da26bc31b3de
ginja <davidevans1985@gmail.com>
2017-01-22 22:26:21 +0000

    Add Goal Trigger Reset Game

---
+++ Assets/Scripts/GoalController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Pong")
+            return;
+
+        var controller = collision.gameObject.GetComponent<PongController>();
+
+        controller.Reset();
+    }
+}
--- Assets/Scripts/PongController.cs
+++ Assets/Scripts/PongController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PongController : MonoBehaviour
 {
@@ -7,7 +8,7 @@ public class PongController : MonoBehaviour
     void Start()
     {
         pongBody = GetComponent<Rigidbody2D>();
-        pongBody.AddForce(new Vector2(700, 0));
+        pongBody.AddForce(new Vector2(1000, 0));
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -15,9 +16,15 @@ public class PongController : MonoBehaviour
         if (coll.gameObject.tag != "Player")
             return;
 
-        var hitAdjust = 
-            (transform.position.y - coll.gameObject.transform.position.y) * 100;
+        var hitAdjust =
+            (transform.position.y - coll.gameObject.transform.position.y) * 200;
 
         pongBody.AddForce(new Vector2(0, hitAdjust));
+    }
+
+    public void Reset()
+    {
+        pongBody.velocity = new Vector2(0, 0);
+        transform.position = new Vector2(0, 0);
     }
 }
