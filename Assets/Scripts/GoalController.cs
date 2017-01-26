d5e230e8a013be02cc74bae2438e95e860d0eb2f
ginja <davidevans1985@gmail.com>
2017-01-26 22:23:04 +0000

    Add Goals With Score & Reset
    - Add collision behind player to simulate the goal
    - When pong collides with goal reset game and increment score
    - Spacebar is kickoff restart
    - Add random direction serve function to pong
    - Tweak pong velocity and hit adjust to keep even speed

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalController : MonoBehaviour
 {
+    public Text ScoreText;
+
+    private int score;
+
+    private void Start()
+    {
+        ScoreText.text = "0";
+        score = 0;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag != "Pong")
             return;
 
-        var controller = collision.gameObject.GetComponent<PongController>();
+        score++;
+        ScoreText.text = score.ToString();
 
+        var controller = collision.gameObject.GetComponent<PongController>();
         controller.Reset();
     }
 }
--- Assets/Scripts/PongController.cs
+++ Assets/Scripts/PongController.cs
@@ -1,30 +1,51 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PongController : MonoBehaviour
 {
+    public float Speed = 30.0f;
+
     private Rigidbody2D pongBody;
-
-    void Start()
-    {
-        pongBody = GetComponent<Rigidbody2D>();
-        pongBody.AddForce(new Vector2(1000, 0));
-    }
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag != "Player")
-            return;
-
-        var hitAdjust =
-            (transform.position.y - coll.gameObject.transform.position.y) * 200;
-
-        pongBody.AddForce(new Vector2(0, hitAdjust));
-    }
 
     public void Reset()
     {
         pongBody.velocity = new Vector2(0, 0);
         transform.position = new Vector2(0, 0);
+    }
+
+    private void Start()
+    {
+        pongBody = GetComponent<Rigidbody2D>();
+        Serve();
+    }
+
+    private void FixedUpdate()
+    {
+        pongBody.velocity = pongBody.velocity.normalized * Speed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+            Serve();
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "Player")
+            return;
+
+        var hitAdjust = (transform.position.y - coll.gameObject.transform.position.y) * 3;
+
+        pongBody.AddForce(new Vector2(0, hitAdjust));
+    }
+
+    private void Serve()
+    {
+        var direction = Random.Range(0, 2) * 2 - 1;
+        var x = Random.Range(0.5f, 1.0f) * direction;
+        var y = Random.Range(0.0f, 0.5f);
+
+        pongBody.velocity = new Vector2(x, y);
+        pongBody.velocity = pongBody.velocity.normalized * Speed;
     }
 }
