9bccddf17a8d9095be1ba2b20643974ecb10d4a1
David Evans <david.evans@ideagen.com>
2017-01-30 16:25:51 +0000

    Add Game Main Menu
    - Add new scene to contain the menu view
    - Add script to control the menu
    - Change game font to Orbitron

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    private int _menuSelectionState = 0;
+
+    private RectTransform _rectTransform;
+
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            switch (_menuSelectionState)
+            {
+                case 0:
+                    _rectTransform.anchoredPosition = new Vector2(-100, -30);
+                    _menuSelectionState = 1;
+                    return;
+                case 1:
+                    _rectTransform.anchoredPosition = new Vector2(-100, 0);
+                    _menuSelectionState = 0;
+                    return;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            switch (_menuSelectionState)
+            {
+                case 0:
+                    _rectTransform.anchoredPosition = new Vector2(-100, -30);
+                    _menuSelectionState = 1;
+                    return;
+                case 1:
+                    _rectTransform.anchoredPosition = new Vector2(-100, 0);
+                    _menuSelectionState = 0;
+                    return;
+            }
+        }
+    }
+}
